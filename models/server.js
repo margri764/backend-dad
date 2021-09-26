51453566df13e62d15ef5966cf0d2d28cc3405af
margri764 <62308899+margri764@users.noreply.github.com>
2021-09-25 21:04:48 -0300

    admin

--- models/server.js
+++ models/server.js
@@ -22,7 +22,7 @@ class Server{
     middlewares(){
         this.app.use(cors());
         this.app.use (express.json());
-        this.app.use(express.static('../public'));
+        this.app.use(express.static('public'));
         this.app.use(fileUpload({
             useTempFiles : true,
             tempFileDir : '/tmp/',
