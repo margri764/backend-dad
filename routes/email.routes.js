25095f15a7a7503736adacb06d9cbc58be869ce6
margri764 <62308899+margri764@users.noreply.github.com>
2021-10-06 23:49:26 -0300

    so close

--- routes/email.routes.js
+++ routes/email.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require ('express');
 const router = Router();
 
-const email = require ('../controllers/email.controllers')
+const { email } = require ('../controllers/email.controllers')
 
 router.post('/', email)
 
